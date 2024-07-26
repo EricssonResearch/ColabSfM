5af69697d14ac3c6fd439aaf0f220fc6097a9f24
Johan Edstedt <johan.edstedt@liu.se>
2024-07-25 23:05:22 -0700

    more fixing

--- hloc/extract_features.py
+++ hloc/extract_features.py
@@ -282,7 +282,12 @@ def main(
         logger.info("Skipping the extraction.")
         return feature_path
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if torch.cuda.is_available():
+        device = 'cuda'
+    elif torch.backends.mps.is_available():
+        device = 'mps'
+    else:
+        device = 'cpu'
     Model = dynamic_load(extractors, conf["model"]["name"])
     model = Model(conf["model"]).eval().to(device)
 
