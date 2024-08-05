fc8ef2a631d432bda780076487a2f1788b56a44e
Johan Edstedt <johan.edstedt@liu.se>
2024-08-05 02:38:37 +0200

    update downloads and reqs

--- scripts/pipelines/7scenes_pipeline/0_7scenes_download.bash
+++ scripts/pipelines/7scenes_pipeline/0_7scenes_download.bash
@@ -1,7 +1,8 @@
-export dataset=data/7scenes
+export dataset="datasets/7scenes"
 for scene in chess fire heads office pumpkin redkitchen stairs; \
-do wget http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/$scene.zip -P $dataset \
-&& unzip $dataset/$scene.zip -d $dataset && unzip $dataset/$scene/'*.zip' -d $dataset/$scene; done
+do wget "http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/${scene}.zip" -P ${dataset} \
+&& unzip ${dataset}/${scene}.zip -d $dataset && unzip $dataset/$scene/'*.zip' -d $dataset/$scene; done
 
-gdown -O $dataset/7scenes_sfm_triangulated.zip 1cu6KUR7WHO7G4EO49Qi3HEKU6n_yYDjb 
+sattler_sfm=7scenes_sfm_triangulated.zip
+gdown -O $dataset/$sattler_sfm 1cu6KUR7WHO7G4EO49Qi3HEKU6n_yYDjb && unzip ${dataset}/$sattler_sfm
 gdown -O $dataset/7scenes_densevlad_retrieval_top_10.zip 1IbS2vLmxr1N0f3CEnd_wsYlgclwTyvB1
